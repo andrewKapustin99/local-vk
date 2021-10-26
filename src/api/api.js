21c56c47add740aeccdf079bc7eb0d089b3aeeb5
Andrew Kapustin <7280567-andrew_kapustin@users.noreply.gitlab.com>
2021-10-26 14:39:06 +0300

    qwe

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import * as axios from "axios";
 export const instance = axios.create({
     withCredentials: true,
     headers: {
-        "API-KEY": 'bee1dfe1-3445-4dd2-a280-df119f81528e'
+        "API-KEY": 'e601c3f7-02ca-4fd8-833e-9dea4cc8e994'
     },
     baseURL: 'https://social-network.samuraijs.com/api/1.0/'
 })
